dc4af1e1cb802f143f8f04badf5aeee73ce4c17d
Siddward <33522644+TripleS2307@users.noreply.github.com>
2019-09-29 19:49:38 -0500

    grammar changes

--- out/production/BasicAntlrScript/project1/rules.g4
+++ out/production/BasicAntlrScript/project1/rules.g4
@@ -10,15 +10,15 @@ query : relation_name '<-' expr;
 // Batch 4
 show_cmd : 'SHOW' atomic_expr;
 create_cmd : 'CREATE TABLE' relation_name '(' typed_attribute_list ')' 'PRIMARY KEY' '(' attribute_list ')';
-update_cmd : 'UPDATE' relation_name 'SET ' attribute_name '=' literal (',' attribute_name '=' literal)* 'WHERE' condition;
-insert_cmd 	: 'INSERT INTO' relation_name 'VALUES FROM' literal ( ',' literal)* | 'INSERT INTO' relation_name 'VALUES FROM RELATION' expr;
+update_cmd : 'UPDATE' relation_name 'SET ' attribute_name '=' '(' literal (',' attribute_name '=' literal)* ')' 'WHERE' condition;
+insert_cmd 	: 'INSERT INTO' relation_name 'VALUES FROM' '(' literal ( ',' literal)* ')' | 'INSERT INTO' relation_name'VALUES FROM RELATION' expr;
 delete_cmd 	: 'DELETE FROM' relation_name 'WHERE' condition;
 
 //Batch 3
 expr : atomic_expr | selection | projection | renaming | union | difference | product | natural_join;
 atomic_expr : relation_name | '(' expr ')';
 selection : 'select' '(' condition ')' atomic_expr;
-projection: 'project' '(' attribute_list '(' atomic_expr;
+projection: 'project' '(' attribute_list ')' atomic_expr;
 renaming : 'rename' '(' attribute_list ')' atomic_expr;
 union : atomic_expr '+' atomic_expr;
 difference : atomic_expr '-' atomic_expr;
--- src/project1/antlr4/MyRulesBaseListener.java
+++ src/project1/antlr4/MyRulesBaseListener.java
@@ -1,13 +1,17 @@
 package project1.antlr4;
+import org.antlr.v4.runtime.tree.ParseTree;
 import project1.Dbms;
 
 import java.sql.SQLOutput;
+import java.util.List;
 
 public class MyRulesBaseListener extends RulesBaseListener {
     public MyRulesBaseListener() {
         Dbms myDbms = new Dbms();
     }
+    /*
     @Override public void enterShow_cmd(RulesParser.Show_cmdContext ctx) {
+        List<ParseTree> children = ctx.children;
         System.out.println("ENTER SHOW");
     }
     @Override public void exitShow_cmd(RulesParser.Show_cmdContext ctx) {
@@ -22,13 +26,45 @@ public class MyRulesBaseListener extends RulesBaseListener {
     @Override public void enterCreate_cmd(RulesParser.Create_cmdContext ctx) {
         System.out.println("ENTER CREATE TABLE");
     }
+    */
+
     @Override public void exitCreate_cmd(RulesParser.Create_cmdContext ctx) {
-        System.out.println("EXIT CREATE TABLE");
+        List<ParseTree> children = ctx.children;
+        ParseTree second_child = children.get(1);
+        String table_name = second_child.getText();
+        ParseTree fourthChild = children.get(3);
+        String table_fields = fourthChild.getText();
+        ParseTree sixthChild = children.get(5);
+        String primary_key = sixthChild.getText();
+        //dbms.createTable(table_name,table_fields,primary_key)
     }
+    @Override public void exitInsert_cmd(RulesParser.Insert_cmdContext ctx) {
+        List<ParseTree> children = ctx.children;
+        String line = "";
+
+        for(ParseTree x :children){
+
+                System.out.println( x.getText());
+
+        }
+
+        //ParseTree second_child = children.get(1);
+        //String table_name = second_child.getText();
+        //ParseTree sixth_child = children.get(5);
+        //String values = sixth_child.getText();
+        //System.out.println("table_name: " + table_name);
+        //System.out.println("values: " + values);
+        System.out.println("\n\n");
+        //dbms.insertInto(table_name,values)
+    }
+
+    /*
     @Override public void enterProgram(RulesParser.ProgramContext ctx) {
         System.out.println("ENTER PROGRAM");
     }
     @Override public void exitProgram(RulesParser.ProgramContext ctx) {
         System.out.println("EXIT PROGRAM");
     }
+    */
+
 }
