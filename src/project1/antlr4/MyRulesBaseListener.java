ec7d69c6cc24c56029d815f46baddbbcfaa93eb0
Siddward <33522644+TripleS2307@users.noreply.github.com>
2019-09-29 23:29:37 -0500

    Sid modifed

--- src/project1/antlr4/MyRulesBaseListener.java
+++ src/project1/antlr4/MyRulesBaseListener.java
@@ -1,4 +1,5 @@
 package project1.antlr4;
+import javafx.scene.Parent;
 import project1.dbms;
 import org.antlr.v4.runtime.tree.ParseTree;
 
@@ -40,10 +41,38 @@ public class MyRulesBaseListener extends rulesBaseListener{
     @Override public void exitInsert_cmd(rulesParser.Insert_cmdContext ctx) {
         List<ParseTree> children = ctx.children;
         String line = "";
+//        ParseTree x = children.get(0);
+//        ParseTree x = children.get(0);
+//        System.out.println( x.get(children.size()-1));
+        for (ParseTree x : children){
+            line += x.getText() + " ";
+        }
+        //System.out.println(line);
+        String tableName = children.get(1).getText();
+        boolean insertTypeRel = false;
+        if (line.contains("RELATION")){
+            insertTypeRel = true;
+        }
+        System.out.println("insertTypeRel: " + insertTypeRel);
+        if (insertTypeRel){
+            String expr =  children.get(children.size()-1).getChild(0).getChild(0).getText();
+            System.out.println("expr: " + expr);
+            switch(expr) {
+                case "project":
+                    // shunting algorithm here!
+                    String attrList =  children.get(children.size()-1).getChild(0).getChild(2).getText();
+                    System.out.println("attrList: " + attrList);
+                    // dbms.insertProject(table_name, attrList);
+                    // code block
+                    break;
+                case "select":
+                    String condition = children.get(children.size()-1).getChild(0).getChild(2).getText();
+                    //dbms
+                    break;
 
-        for(ParseTree x :children){
-
-            System.out.println( x.getText());
+                    default:
+                    break;
+            }
 
         }
 
