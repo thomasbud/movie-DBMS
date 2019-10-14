8fcb3ba63d7b814251ec26f0f227b8701da57597
Siddward <33522644+TripleS2307@users.noreply.github.com>
2019-10-13 20:21:25 -0500

    imported movieParser

--- src/project1/Main.java
+++ src/project1/Main.java
@@ -1,10 +1,15 @@
 package project1;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import csce315.project1.Credits;
+import csce315.project1.Movie;
+import csce315.project1.MovieDatabaseParser;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
 import project1.antlr4.MyRulesBaseListener;
 import project1.antlr4.rulesLexer;
 import project1.antlr4.rulesParser;
@@ -15,7 +20,28 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+
+
+        MovieDatabaseParser parser2 = new MovieDatabaseParser();
+
+        List<Movie> moviesList = parser2.deserializeMovies("C:/Users/sidds/Downloads/movies_single.json");
+        List<Credits> creditsList = parser2.deserializeCredits("C:/Users/sidds/Downloads/credits_single.json");
+
+        Movie movie = moviesList.get(0);
+        Credits credits = creditsList.get(0);
+
+        System.out.println("Is it an adult film? ");
+        System.out.println(movie.getAdult());
+        System.out.println();
+
+        System.out.println("What language was the movie in?");
+        System.out.println(movie.getSpoken_languages().get(0).getName());
+
+
+
+/*
+
         File file = new File("src/project1/input.txt");
         Scanner scanner = new Scanner(file);
         List<String> lines = new ArrayList<>();
@@ -36,7 +62,10 @@ public class Main {
             parser.removeErrorListeners();
             rulesParser.ProgramContext programContext = parser.program();
             ParseTreeWalker walker = new ParseTreeWalker();
-            walker.walk(listener, programContext);
+            walker.walk((ParseTreeListener) listener, programContext);
         }
+    }
+
+ */
     }
 }
